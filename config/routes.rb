dc7ab074a1fb8c4a33715a892d98837cabd82430
Nao <dece.0.na.12@gmail.com>
2020-06-07 12:44:53 +0900

    indexの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-#一覧画面へのルーティング
- get '/users', to:'users#index' 
-#新規投稿画面 へのルーティング
- get '/users/new', to:'users#new'
- #新規登録を行うためのルーティング
- post '/users', to: 'users#create'
- #編集画面へのルーティング
- get '/users/:id/edit', to:'users#edit'
- #更新ルーティング
- patch '/users/:id', to:'users#update'
- #削除ルーティング
- delete '/users/:id', to: 'users#destroy'
+  #一覧画面へのルーティング
+  get "/users", to: "users#index"
+  root "users#index"
+
+  #新規投稿画面 へのルーティング
+  get "/users/new", to: "users#new"
+  #新規登録を行うためのルーティング
+  post "/users", to: "users#create"
+  #編集画面へのルーティング
+  get "/users/:id/edit", to: "users#edit"
+  #更新ルーティング
+  patch "/users/:id", to: "users#update"
+  #削除ルーティング
+  delete "/users/:id", to: "users#destroy"
 end
